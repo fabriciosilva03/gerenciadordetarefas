798f1c9caa503526727869cf31b1a778284da330
fabriciosilva03 <fabriciosilva03@hotmail.com>
2021-03-08 16:50:29 -0300

    Funcionalidades implementadas, remoção de tarefas, armazenando iformações no dispositivo.

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
+
 import { 
   StyleSheet, 
   Text, 
@@ -8,7 +11,9 @@ import {
   FlatList,
   KeyboardAvoidingView,
   Platform,
-  Keyboard, 
+  Keyboard,
+  Alert,
+
 } from 'react-native';
 
 import {Ionicons, MaterialIcons} from "@expo/vector-icons"
@@ -21,16 +26,67 @@ import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 
 
 export default function App() {
-  const [task, setTask] = useState(['alisson' , 'renam', 'macedo']);
+  const [task, setTask] = useState([]);
   const [newTask, setNewTask] = useState('');
 
 async function addTask(){
+
+  if(newTask === ''){
+    return;
+  }
+
+  const search = task.filter(task => task === newTask);
+
+  if(search.length  != 0){
+    Alert.alert("Atenção", "Descrição da tarefa repetida!");
+    return;
+  }
 
   setTask([ ... task , newTask]);
   setNewTask("");
 
   Keyboard.dismiss();
 }
+
+async function removeTask(item){
+  Alert.alert(
+    "Deletar Task", 
+    "Tem certeza que deseja remover esta anotação!",
+    [
+      {
+        text: "Cancel",
+        onPress: () => {
+          return;
+        },
+        style: 'cancel'
+      },
+      {
+        text: 'OK',
+        onPress: () => setTask(task.filter(tasks => tasks != item))
+      }
+    ],
+    { cacelable: false}
+  );
+  
+}
+
+useEffect(() => {
+  async function carregaDados(){
+    const task = await AsyncStorage.getItem("task");
+
+    if(task){
+      setTask(JSON.parse(task));
+    }
+  }
+  carregaDados();
+}, [])
+
+useEffect(() => {
+  async function salvaDados(){
+    AsyncStorage.setItem("task", JSON.stringify(task))
+  }
+  salvaDados();
+}, [task] )
 
 {/*
 useEffect(() => {
@@ -59,7 +115,7 @@ useEffect(() => {
             renderItem={({ item }) =>  (
               <View style={styles.ContainerView}>
                 <Text style={styles.Texto}>{item}</Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => removeTask(item)}>
                     <MaterialIcons name="delete-forever" size={25} color="#f64c75" />
                 </TouchableOpacity>
               </View>
